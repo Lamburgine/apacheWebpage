32eab91edaff31ad323aeb94a5f000e1901acf9d
Lamburgine <133622170+Lamburgine@users.noreply.github.com>
2023-07-11 21:37:26 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 d<pre>
 <?php
-echo "<body style='background-color:red'>";
+echo "<body style='background-color:blue'>";
 print "welcome to webserver Created by Terraform!!!!<br />";
 print `/usr/sbin/ifconfig`;
 ?>
